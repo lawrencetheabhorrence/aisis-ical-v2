7da6c08051df642e7cd1ecc9a447695d3822701b
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-03-10 17:57:34 +0800

    initial commit

---
+++ entrypoints/content.ts
@@ -0,0 +1,8 @@
+import dayjs from "dayjs";
+export default defineContentScript({
+  matches: ['*://*.google.com/*'],
+  main() {
+    console.log('Hello content.');
+    console.log(dayjs());
+  },
+});
