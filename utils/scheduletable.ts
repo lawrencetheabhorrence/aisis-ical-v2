5044551dfb62790ab00bd80aa5df080279133a82
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-03-17 22:41:07 +0800

    Removed try catch for connecting events

--- utils/scheduletable.ts
+++ utils/scheduletable.ts
@@ -32,32 +32,27 @@ export function mergeCellsInColumn(cells: EventColumn): EventColumn {
     s1: IntermediateEventData,
     s2: IntermediateEventData,
   ): IntermediateEventData {
-    if (!isEventSameSubject(s1, s2)) {
-      throw new Error("Events are not the same subject");
-    }
-
     if (s1.start.isAfter(s2.start, "minute")) {
       return connectTwoEventsByTime(s2, s1);
     }
 
-    if (!s1.end.isSame(s2.start, "minute")) {
-      throw new Error("These two events do not connect.");
-    }
 
     const result = { ...s1 };
     result.end = s2.end;
     return { ...s1, end: s2.end };
   }
 
+  function isConnecting(s1: IntermediateEventData, s2: IntermediateEventData) {
+    return s1.end.isSame(s2.start, "minute") || s2.end.isSame(s1.start, "minute");
+  }
+
   let currentEvent = cells[0];
 
   for (let i = 1; i < cells.length; ++i) {
-    try {
-      currentEvent = connectTwoEventsByTime(currentEvent, cells[i]);
-    } catch (error) {
-      merged.push(currentEvent);
-      currentEvent = cells[i];
-    }
+    if (!isEventSameSubject(currentEvent, cells[i]) || !isConnecting(currentEvent, cells[i]))
+      break;
+
+    currentEvent = connectTwoEventsByTime(currentEvent, cells[i]);
   }
 
   merged.push(currentEvent);
