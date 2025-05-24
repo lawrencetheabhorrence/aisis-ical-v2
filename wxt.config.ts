8ddc30092f8b9d6ea31030db2e26ac8fd48d7436
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-05-24 17:11:02 +0800

    config changes

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,4 +3,11 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   extensionApi: "chrome",
+  manifest: {
+    browser_specific_settings: {
+      gecko: {
+        id: "aisis-ical@aisis-ical.com"
+      }
+    }
+  }
 });
