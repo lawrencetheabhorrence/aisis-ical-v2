dcd77a7566f7348e893e40df1edc24ddc3daaef9
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-12-26 14:55:33 +0800

    updated constants

--- utils/constants.ts
+++ utils/constants.ts
@@ -10,8 +10,8 @@ export const endDate: Record<string, Dayjs> = {
   "2025-1": dayjs("20251210"),
   "2025-2": dayjs("20260523"),
 };
-export const nextSem = "2025-2";
-export const nowSem = "2025-1";
+export const nextSem = "2026-0";
+export const nowSem = "2025-2";
 
 export const weekdayNumValue: Record<ICalWeekday, number> = {
   SU: 0,
@@ -29,7 +29,7 @@ export const startDate: Record<string, Dayjs> = {
   "2024-2": dayjs("2025-01-15"),
   "2025-0": dayjs("2025-06-09"),
   "2025-1": dayjs("2025-05-08"),
-  "2025-2": dayjs("2026-01-12"),
+  "2025-2": dayjs("2026-01-07"),
 };
 
 export const weekdaysDict: Record<string, ICalWeekday> = {
