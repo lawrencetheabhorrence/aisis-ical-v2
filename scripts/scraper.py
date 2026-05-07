8260b9ead9322f4c201ffe12148553983bbd7a1d
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2026-05-07 14:29:45 +0800

    scraper

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -6,13 +6,22 @@ import requests as r
 import datetime as dt
 import os
 import csv
-from fake_usergent import UserAgent
+from pathlib import Path
+from fake_useragent import UserAgent
 
 present_year = dt.date.today().year
 school_years = [
     f"{present_year - 1}-{present_year}",
     f"{present_year}-{present_year + 1}",
 ]
+
+
+def get_csv_path(sy: str):
+    return Path(".") / f"{sy}-dates.csv"
+
+
+def get_html_path(sy: str):
+    return Path(".") / f"academic-calendar-{sy}.html"
 
 
 def fetch_calendar(sy):
@@ -22,13 +31,17 @@ def fetch_calendar(sy):
         headers={"User-Agent": ua.random},
     )
     if req.status_code == 200:
-        with open(f"./{sy}-backup.html") as f:
+        with open(get_html_path(sy), "w") as f:
             f.write(req.text)
+    else:
+        raise ConnectionError("Fetch failed")
 
 
 def get_dates_from_row(first_cell_text, soup):
     first_cell = soup.find(re.compile(r".+"), string=first_cell_text)
-    row = first_cell.parent.parent
+    row = first_cell
+    while row.tag != "tr":
+        row = row.parent
     row_strings = list(row.stripped_strings)
 
     date_strings = []
@@ -42,16 +55,25 @@ def get_dates_from_row(first_cell_text, soup):
 
 
 def get_dates_from_calendar(sy, calendar_html):
+    cal_html_text = ""
     with open(calendar_html, "r") as cal_html:
         cal_html_text = cal_html.read()
+
+    if len(cal_html_text) > 0:
+        cal_html_text = unicodedata.normalize("NFD", cal_html_text)
+        print("parsing for ", sy)
         soup = BeautifulSoup(cal_html_text)
         start_dates = get_dates_from_row("Start of the Semester/Term", soup)
-        end_class_dates = get_dates_from_row(
-            "Last day of regular classes for the   Semester/Term", soup
-        )
+        print(start_dates)
+        # end_class_dates = get_dates_from_row(
+        #     re.compile(r"Last day of regular classes.+"), soup
+        # )
+        # print(end_class_dates)
         end_dates = get_dates_from_row("Last day of the Semester/Term", soup)
+        print(end_dates)
 
-        with open(f"./{sy}-dates.csv", "w") as dates_csv:
+        with open(get_csv_path(sy), "w") as dates_csv:
+            print("Writing csv for ", sy)
             dates_writer = csv.writer(dates_csv, delimiter=",")
             dates_writer.writerow(["Time of Sem", "Sem 0", "Sem 1", "Sem 2"])
             dates_writer.writerow(["Start"] + start_dates)
@@ -60,12 +82,14 @@ def get_dates_from_calendar(sy, calendar_html):
 
 
 for sy in school_years:
-    if os.path.exists(f"./{sy}-dates.csv"):
+    if os.path.exists(get_csv_path(sy)):
         continue
-    if not (os.path.exists(f"./{sy}-backup.html")):
-        html_calendar = fetch_calendar(sy)
-    else:
-        with open(f"./{sy}-backup.html") as html_cal_file:
-            html_calendar = html_cal_file.read()
+    html_calendar = get_html_path(sy)
+    if not (os.path.exists(get_html_path(sy))):
+        try:
+            html_calendar = fetch_calendar(sy)
+        except ConnectionError:
+            print("Failed to fetch calendar")
+            continue
 
     get_dates_from_calendar(sy, html_calendar)
